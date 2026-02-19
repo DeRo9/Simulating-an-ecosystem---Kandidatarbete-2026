e75e3b8c22ddaf13a19d29a20160ef71d86a6eef
Elliot Lund <elliotlund03@gmail.com>
2026-02-19 11:57:05 +0100

    Updated behaviours
    Update MooseBehaviour + added a general AnimalBehaviour script for inheritance.

--- Assets/Scripts/AnimalBehaviours/MooseBehaviour.cs
+++ Assets/Scripts/AnimalBehaviours/MooseBehaviour.cs
@@ -1,105 +1,25 @@
+using System.IO.Pipes;
 using Unity.VisualScripting;
 using UnityEditor.AdaptivePerformance.Editor;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem.Android;
 
-public class MooseBehaviour : MonoBehaviour
+public class MooseBehaviour : AnimalBehaviour
 {
-    // Components
-    private Rigidbody rb;
-    private Animator anim;
-    private NavMeshAgent agent;
 
-    // Home area for the moose
-    public Area HomeArea;
-
-    // Internal state of the moose
-    enum State
-    {
-        Idle,
-        Wander,
-        Eat,
-    }
-
-    [SerializeField]
-    State CurrentState = State.Idle;
 
     MooseNeeds needs;
-
-    // Waiting timers
-    [SerializeField]
-    float minTimeWaiting = 2f;
-
-    [SerializeField]
-    float maxTimeWaiting = 5f;
-
-    [SerializeField]
-    float waitTime = 0f;
-
     GameObject foodTarget;
     float foodDetectionRadius = 20f;
 
-    void Start()
+    protected override void Start()
     {
+        base.Start();
         needs = GetComponent<MooseNeeds>();
-
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
-        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Checks if the moose has reached its destination
-    bool hasArrived()
-    {
-        return agent.remainingDistance <= agent.stoppingDistance;
-    }
 
-    // Changes the state of the moose and updates behavior accordingly
-    void ChangeState(State newState)
-    {
-        CurrentState = newState;
-        switch (CurrentState)
-        {
-            case State.Idle:
-                waitTime = Random.Range(minTimeWaiting, maxTimeWaiting); // Random waiting time between min and max
-                agent.isStopped = true;
-                break;
-            case State.Wander:
-                agent.isStopped = false;
-                agent.SetDestination(HomeArea.GetRandomPoints());
-                break;
-            case State.Eat:
-                if (foodTarget != null)
-                {
-                    agent.isStopped = false;
-                    agent.SetDestination(foodTarget.transform.position);
-                }
-
-                break;
-        }
-
-    }
-
-    void Update()
-    {
-        // Update animation based on movement
-        anim.SetBool("isWalking", agent.velocity.magnitude > 0.1f); // "isWalking" är en bool i animator
-
-
-        switch (CurrentState)
-        {
-            case State.Idle:
-                UpdateIdle();
-                break;
-            case State.Wander:
-                UpdateWander();
-                break;
-            case State.Eat:
-                UpdateEat();
-                break;
-        }
-    }
 
     // Finds the closest food item within the detection radius and sets it as the target
     bool FindFood()
@@ -134,7 +54,27 @@ public class MooseBehaviour : MonoBehaviour
         return false;
     }
 
-    void UpdateWander()
+    protected override bool IsHungry()
+    {
+        // Moose is hungry, find food
+        if (needs.isHungry)
+        {
+            return FindFood();
+        }
+        return false;
+    }
+
+    protected override void EatStateForSpecificAnimal()
+    {
+        if (foodTarget != null)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(foodTarget.transform.position);
+        }
+    }  
+
+
+    protected override void UpdateWander()
     {
         if (hasArrived()) // If the moose has reached its destination, switch to idle state
         {
@@ -142,13 +82,8 @@ public class MooseBehaviour : MonoBehaviour
         }
     }
 
-    void UpdateIdle()
+    protected override void UpdateIdle()
     {
-        // Moose is hungry, find food
-        if (needs.isHungry && FindFood())
-        {
-            ChangeState(State.Eat);
-        }
 
         waitTime -= Time.deltaTime; // Decrease waiting time
         if (waitTime < 0f) // To occasionally switch to wandering
@@ -157,7 +92,7 @@ public class MooseBehaviour : MonoBehaviour
         }
     }
 
-    void UpdateEat()
+    protected override void UpdateEat()
     {
         // If the food target is null, switch back to wandering
         if (foodTarget == null)
