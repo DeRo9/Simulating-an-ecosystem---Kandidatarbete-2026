9ad9391716aae75ee9da1e041a811cbd8543abf9
Amar Bosnjak <amarb@student.chalmers.se>
2026-03-07 02:12:55 +0100

    inital commits with  wolf hunt added, needs to be modified

--- Assets/Scripts/Animals/Bear/BearBehaviour.cs
+++ Assets/Scripts/Animals/Bear/BearBehaviour.cs
@@ -4,7 +4,29 @@ public class BearBehaviour : AnimalBehaviour
 {
     GameObject foodTarget;
     BearHearing hearing;
+    BearFOV fov;
+    GameObject preyTarget;
+    public GameObject CurrentTarget => preyTarget;
+    float attackRange = 3.5f; // Range within which the wolf can attack prey
 
+    float huntCooldown = 5f; // Time the wolf must wait after giving up on a hunt before it can hunt again
+    [SerializeField]
+    float huntCooldownTimer = 0;
+
+    float repathTimer = 0f;
+    float repathInterval = 0.3f; // Time interval for recalculating path to prey
+
+    float attackTimer = 0f;
+    float attackInterval = 1f; // Time interval for attacking, to prevent multiple attacks in quick succession
+
+    float foodSearchingCooldown;
+
+    float deathWaitTimer = 0f;
+    float deathWaitDuration = 2f; // Timer to wait before eating, otherwise the moose will instantly be eaten (Not leting the animation finish).
+    bool waitingForDeathAnimation = false;
+
+    GameObject foodTarget;
+    GameObject pendingCarcass;
     protected override void Start()
     {
         base.Start();
@@ -69,6 +91,178 @@ public class BearBehaviour : AnimalBehaviour
         return false;
     }
 
+    bool FindPrey()
+    {
+
+        if (preyTarget != null)
+            return true; // If the wolf already has a target, don't change prey
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, animal.sightRange);
+
+        float closestDistance = Mathf.Infinity;
+        GameObject closestPrey = null;
+
+        foreach (Collider hit in hits)
+        {
+
+            if (!fov.IsInFOV(hit.transform))
+                continue; // Skip if the collider is not in the wolf's field of view
+
+
+            if (hit.CompareTag("Moose"))
+            {
+                MooseBehaviour moose = hit.GetComponentInParent<MooseBehaviour>();
+                if (moose == null || moose.isDead) continue; // Skip if moose is already dead.
+
+                float distance = Vector3.Distance(transform.position, hit.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestPrey = hit.gameObject;
+                }
+
+            }
+
+        }
+
+        if (closestPrey != null)
+        {
+            preyTarget = closestPrey;
+
+            MooseBehaviour moose = preyTarget.GetComponentInParent<MooseBehaviour>();
+            if (moose != null)
+            {
+                moose.OnBeingHunted(gameObject); // Notify the moose that it is being hunted
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    protected override void HuntState()
+    {
+        if (preyTarget != null)
+        {
+            agent.isStopped = false;
+            agent.speed = animal.runningSpeed; // Set speed to hunt speed
+            agent.SetDestination(preyTarget.transform.position);
+        }
+        else
+        {
+            agent.speed = animal.speed; // Reset speed to normal
+            ChangeState(State.Wander); // If the prey is lost, switch to wandering
+        }
+    }
+
+    protected override void UpdateHunt()
+    {
+
+        if (preyTarget == null)
+        {
+            ChangeState(State.Wander);
+            return; // If the wolf has no prey, switch to wandering
+        }
+
+        MooseBehaviour moose = preyTarget.GetComponentInParent<MooseBehaviour>();
+        if (moose != null && moose.isDead)
+        {
+            notifyDeath();
+        }
+
+        if (preyTarget != null)
+        {
+            if (!fov.IsInFOV(preyTarget.transform))
+            {
+                LostPrey(); // If the prey is no longer in the wolf's field of view, give up
+                return;
+            }
+
+            // Check if the prey is still within sight range
+            float distance = Vector3.Distance(transform.position, preyTarget.transform.position);
+            if (distance > animal.sightRange)
+            {
+                LostPrey(); // If the prey is too far away, give up
+                return;
+            }
+
+            // Attack if within range
+            if (distance <= attackRange)
+            {
+                agent.SetDestination(preyTarget.transform.position);
+                AttackOnContact();
+            }
+            else
+            {
+
+                // Keep moving towards the prey
+                agent.isStopped = false;
+                repathTimer += Time.deltaTime;
+
+                if (repathTimer >= repathInterval) // Stuttering prevention: Recalculate path to prey at regular intervals
+                {
+                    agent.SetDestination(preyTarget.transform.position);
+                    repathTimer = 0f;
+                }
+
+            }
+
+        }
+
+        if (needs.noMoreStamina)
+        {   
+            LostPrey(); // If the wolf has no more stamina to run after, give up
+            return;
+        }
+
+    }
+
+    void AttackOnContact()
+    {
+        if (preyTarget != null)
+        {
+            attackTimer += Time.deltaTime;
+
+            if (attackTimer >= attackInterval)
+            {
+                anim.SetTrigger("Attack");
+                attackTimer = 0f;
+                Debug.Log("Wolf attacked prey");
+            }
+        }
+    }
+
+    public void DamageMoose()
+    {
+        if (preyTarget != null)
+        {
+            MooseBehaviour moose = preyTarget.GetComponentInParent<MooseBehaviour>();
+            if (moose != null)
+            {
+                moose.InflictDamage(animal.attackDamage);
+            }
+        }
+    }
+
+    void LostPrey()
+    {
+        //StopAttack(); // Stop attacking if the prey is lost
+
+        if (preyTarget != null)
+        {
+            MooseBehaviour moose = preyTarget.GetComponentInParent<MooseBehaviour>();
+            if (moose != null)
+            {
+                moose.OnNoLongerHunted(gameObject); // Notify the moose that it is no longer being hunted
+            }
+        }
+
+        preyTarget = null; // Give up on the prey after hunting for too long
+        huntCooldownTimer = huntCooldown; // Start cooldown timer
+        agent.speed = animal.speed; // Reset speed to normal
+        ChangeState(State.Wander);
+    }
 
     protected override bool IsHungry()
     {
@@ -137,6 +331,63 @@ public class BearBehaviour : AnimalBehaviour
         }
     }
 
+    public void notifyDeath()
+    {
+        pendingCarcass = preyTarget;
+        preyTarget = null;
+        agent.isStopped = true;
+        waitingForDeathAnimation = true;
+        deathWaitTimer = deathWaitDuration;
+
+    }
+
+    // Finds the closest food item within the detection radius and sets it as the target
+    bool FindCarcass()
+    {
+
+        if (foodSearchingCooldown > 0f)
+        {
+            foodSearchingCooldown -= Time.deltaTime;
+            return foodTarget != null;
+        }
+        foodSearchingCooldown = 0.5f;
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, animal.sightRange);
+
+        float closestDistance = Mathf.Infinity;
+        GameObject closestFood = null;
+
+        foreach (Collider hit in hits)
+        {
+
+
+            if (!fov.IsInFOV(hit.transform))
+            {
+                continue; // Skip if not in FOV
+            }
+
+            Debug.Log("Wolf found carcass.");
+            if (hit.CompareTag("carcass"))
+            {
+                float distance = Vector3.Distance(transform.position, hit.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestFood = hit.gameObject;
+                }
+
+            }
+
+        }
+
+        if (closestFood != null)
+        {
+            foodTarget = closestFood;
+            return true;
+        }
+
+        return false;
+    }
 
 }
 
