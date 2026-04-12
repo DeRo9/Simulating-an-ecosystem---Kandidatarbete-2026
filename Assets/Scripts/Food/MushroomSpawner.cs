9fe890f790f4b2d992c91020be83504475176a55
Elliot Lund <elliotlund03@gmail.com>
2026-04-12 12:38:19 +0200

    some fixes
    timeInterval was not set in the inspector, so that was the problem.
    
    Also, noticed trees were spawning in water, so that is now fixed.

--- Assets/Scripts/Food/MushroomSpawner.cs
+++ Assets/Scripts/Food/MushroomSpawner.cs
@@ -13,7 +13,7 @@ public class MushroomSpawner : MonoBehaviour
 
     [SerializeField] private float SpawnRadius = 100f;
     [SerializeField] private float reproduceRadius = 10f;
-    [SerializeField] private float timeInterval = 500f; // 1 second = 1 hour
+    [SerializeField] private float timeInterval = 50f; // 1 second = 1 hour
 
     private float timer;
     private bool isSimulationRunning = false;
