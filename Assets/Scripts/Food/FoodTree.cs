a32d50e616fa776bd136e96d417dce48c85d789b
Amar Bosnjak <amarb@student.chalmers.se>
2026-05-08 14:55:16 +0200

    Added cooldown

--- Assets/Scripts/Food/FoodTree.cs
+++ Assets/Scripts/Food/FoodTree.cs
@@ -8,13 +8,48 @@ public class FoodTree : MonoBehaviour, IsEdible
     private Species[] allowedSpecies = {Species.moose};
     private MushroomSpawner spawner;
 
+    [SerializeField]
+    private float regrowCooldown = 60f;
+
+    private float cooldownTimer = 0f;
+    private Renderer rend;
+    private Collider col;
+
+
+    void Start()
+    {
+        rend = GetComponentInChildren<Renderer>();
+        col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        if (cooldownTimer > 0f)
+        {
+            cooldownTimer -= Time.deltaTime;
+
+            if (cooldownTimer <= 0f)
+            {
+                //if (rend != null) rend.enabled = true; testing
+                if (col != null) col.enabled = true;
+            }
+        }
+    }
     public float Consume()
     {
+        if (cooldownTimer > 0f){
+            return 0f;
+        } 
+        cooldownTimer = regrowCooldown;
+
+        //if (rend != null) rend.enabled = false; here for testing
+        if(col != null) col.enabled = false;
         return nutritionValue;
     }
 
     public bool CanBeEatenBy(Species species)
     {
+        if (cooldownTimer > 0f ) return false;
         foreach (Species allowed in allowedSpecies)
         {
             if (allowed == species)
